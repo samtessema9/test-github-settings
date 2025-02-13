94bbf21dc1b9457a232361e9b33f249fbf73a4e7
Sam Tessema <samtessema@Sams-MacBook-Air.local>
2025-02-12 16:06:25 -0800

    add route

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Greetings from the /greet route!")
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/greet", greetHandler)
+	
 	fmt.Println("Server starting on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
